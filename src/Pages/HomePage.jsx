6a931ef5a12842f9e02d39b1b4e346bc069bdde6
dskulkarni2006@gmail.com <dskulkarni2006@gmail.com>
2026-08-28 14:36:52 +0530

    feat: implement api structure , call it , not normalised

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,10 +1,44 @@
-import { useState } from "react";
-import SearchBar from "../Components/SearchBar"
+import { useState, useEffect } from "react";
+import SearchBar from "../Components/SearchBar";
 import Tabs from "../Components/Tabs";
+import { fetchGIF, fetchVideos, fetchPhotos } from "../api/mediaApi";
 
 const HomePage = () => {
-    const [query, setQuery] = useState("");
-    const [activeState, setActiveState] = useState("images");
+  const [query, setQuery] = useState("");
+  const [activeState, setActiveState] = useState("images");
+  const [results, setResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!query.trim()) {
+      return;
+    }
+    const searchMedia = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        let data;
+        if (activeState === "images") {
+          data = await fetchPhotos(query);
+        }
+        if (activeState === "videos") {
+          data = await fetchVideos(query);
+        }
+        if (activeState === "gifs") {
+          data = await fetchGIF(query);
+        }
+        setResults(data);
+      } catch (err) {
+        console.error(err);
+        setError("Something went wrong while fetching data...");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    searchMedia();
+  }, [query, activeState]);
   return (
     <main className="home-page">
       <section className="hero-section">
@@ -16,15 +50,15 @@ const HomePage = () => {
         </h1>
 
         <p className="hero-description">
-          Search millions of images, videos and GIFs from multiple
-          sources in one place.
+          Search millions of images, videos and GIFs from multiple sources in
+          one place.
         </p>
 
-        <SearchBar query={query} setQuery={setQuery}/>
-        <Tabs activeState={activeState} setActiveState={setActiveState}/>
+        <SearchBar query={query} setQuery={setQuery} />
+        <Tabs activeState={activeState} setActiveState={setActiveState} />
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
